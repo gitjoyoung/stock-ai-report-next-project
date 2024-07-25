4ceff0801b29970b3e83471a53032fbd5b51fb71
Jo Young <githuborigin@gmail.com>
2024-07-26 08:54:01 +0900

    fix : api 함수 개선

--- src/app/api/report/chart/route.ts
+++ src/app/api/report/chart/route.ts
@@ -1,9 +1,9 @@
-import { StockPrice } from "@/components/Report/type/report/stockType";
-import { getStockPrice } from "@/service/report/stockPriceApi";
-import dayjs from "dayjs";
+import { StockPrice } from '@/components/Report/type/report/stockType';
+import { getStockPrice } from '@/service/report/stockPriceApi';
+import dayjs from 'dayjs';
 
 interface ProcessResult {
-  status: "fulfilled" | "rejected";
+  status: 'fulfilled' | 'rejected';
   value?: StockPrice[];
   reason?: string;
 }
@@ -23,11 +23,11 @@ export async function POST(request: Request) {
   const { code } = await request.json();
 
   const fetchData = Promise.allSettled([
-    getStockPrice(code, "day", "NASDAQ"),
-    getStockPrice(code, "month&range=3", "NASDAQ"),
-    getStockPrice(code, "month&range=12", "NASDAQ"),
-    getStockPrice(code, "year&range=3", "NASDAQ"),
-    getStockPrice(code, "year&range=10", "NASDAQ"),
+    getStockPrice(code, 'day', 'NASDAQ'),
+    getStockPrice(code, 'month&range=3', 'NASDAQ'),
+    getStockPrice(code, 'month&range=12', 'NASDAQ'),
+    getStockPrice(code, 'year&range=3', 'NASDAQ'),
+    getStockPrice(code, 'year&range=10', 'NASDAQ'),
   ]);
 
   const results = await fetchData;
@@ -36,16 +36,16 @@ export async function POST(request: Request) {
     result: ProcessResult,
     format: string,
   ): StockPrice[] | null => {
-    return result.status === "fulfilled" && result.value !== undefined
+    return result.status === 'fulfilled' && result.value !== undefined
       ? stockFormatData(result.value, format)
       : null;
   };
   const allData = {
-    "1일": processResult(results[0], "HH:mm"),
-    "3개월": processResult(results[1], "YY/MM/DD"),
-    "1년": processResult(results[2], "YY/MM"),
-    "3년": processResult(results[3], "YY/MM"),
-    "10년": processResult(results[4], "YY/MM"),
+    '1일': processResult(results[0], 'HH:mm'),
+    '3개월': processResult(results[1], 'YY/MM/DD'),
+    '1년': processResult(results[2], 'YY/MM'),
+    '3년': processResult(results[3], 'YY/MM'),
+    '10년': processResult(results[4], 'YY/MM'),
   };
   return new Response(JSON.stringify(allData));
 }
--- src/app/api/report/stockList/route.ts
+++ src/app/api/report/stockList/route.ts
@@ -1,18 +1,18 @@
-import { StockInfo } from "@/components/Report/type/report/stockType";
-import { firestore } from "@/firebase/firebaseConfig";
-import { doc, getDoc, setDoc } from "firebase/firestore";
+import { StockInfo } from '@/components/Report/type/report/stockType';
+import { firestore } from '@/firebase/firebaseConfig';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 
 export async function GET(request: Request) {
   const url = new URL(request.url);
-  const stockName = url.searchParams.get("stockName");
+  const stockName = url.searchParams.get('stockName');
   if (!stockName) {
-    return new Response("Stock name is missing", { status: 400 });
+    return new Response('Stock name is missing', { status: 400 });
   }
-  const stockRef = doc(firestore, "stockList", stockName); // 문서 참조 생성
+  const stockRef = doc(firestore, 'stockList', stockName); // 문서 참조 생성
   const docSnap = await getDoc(stockRef); // 문서 조회
 
   if (!docSnap.exists()) {
-    return new Response("Stock not found", { status: 404 });
+    return new Response('Stock not found', { status: 404 });
   }
   return new Response(JSON.stringify(docSnap.data()));
 }
@@ -20,14 +20,9 @@ export async function GET(request: Request) {
 export async function POST(request: Request) {
   const req: StockInfo[] = await request.json(); // 요청으로부터 주식 이름과 데이터를 추출
   for (const stock of req) {
-    const stockRef = doc(firestore, "stockList", stock.name); // 문서 참조 생성
+    const stockRef = doc(firestore, 'stockList', stock.name); // 문서 참조 생성
     await setDoc(stockRef, stock);
   }
 
-  return new Response("Stock data has been added successfully.", {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  return new Response(JSON.stringify({ message: 'Stocks added' }));
 }
