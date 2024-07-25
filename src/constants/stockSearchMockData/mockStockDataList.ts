35c49f3a32fa4a7638322e81674241580334c1ef
Jo Young <githuborigin@gmail.com>
2024-07-26 08:53:13 +0900

    refactor : 목업 데이터 추가 중복 제거 맵배열

--- src/constants/stockSearchMockData/mockStockDataList.ts
+++ src/constants/stockSearchMockData/mockStockDataList.ts
@@ -17,6 +17,7 @@ const badStocks: StockInfo[] = [
   { ticker: 'GPRO', name: '고프로', code: 'GPRO.O' },
 ];
 
+// 기본 주식 데이터
 const defaltStocks: StockInfo[] = [
   { ticker: 'TSLA', name: '테슬라', code: 'TSLA.O' },
   { ticker: 'MSFT', name: 'MS', code: 'MSFT.O' },
@@ -24,8 +25,42 @@ const defaltStocks: StockInfo[] = [
   { ticker: 'U', name: '유니티', code: 'U' },
 ];
 
+// 고성장 기술주
+const growthStocks: StockInfo[] = [
+  { ticker: 'FB', name: '페이스북', code: 'FB.O' },
+  { ticker: 'SNAP', name: '스냅챗', code: 'SNAP.O' },
+  { ticker: 'SPOT', name: '스포티파이', code: 'SPOT.O' },
+  { ticker: 'SQ', name: '스퀘어', code: 'SQ.O' },
+  { ticker: 'ZM', name: '줌 비디오 커뮤니케이션즈', code: 'ZM.O' },
+];
+
+// 전통적인 블루칩 주식
+const blueChipStocks: StockInfo[] = [
+  { ticker: 'KO', name: '코카콜라', code: 'KO.N' },
+  { ticker: 'PEP', name: '펩시', code: 'PEP.N' },
+  { ticker: 'JNJ', name: '존슨앤존슨', code: 'JNJ.N' },
+  { ticker: 'PG', name: '프록터앤드갬블', code: 'PG.N' },
+  { ticker: 'XOM', name: '엑손모빌', code: 'XOM.N' },
+];
+
+// 혁신적인 스타트업
+const startupStocks: StockInfo[] = [
+  { ticker: 'BYND', name: '비욘드 미트', code: 'BYND.O' },
+  { ticker: 'ROKU', name: '로쿠', code: 'ROKU.O' },
+  { ticker: 'PLTR', name: '팔란티어', code: 'PLTR.O' },
+  { ticker: 'DOCU', name: '도큐사인', code: 'DOCU.O' },
+  { ticker: 'CRWD', name: '크라우드스트라이크', code: 'CRWD.O' },
+];
+
 // 모든 주식 데이터를 병합
-const allStocks: StockInfo[] = [...goodStocks, ...badStocks, ...defaltStocks];
+const allStocks: StockInfo[] = [
+  ...goodStocks,
+  ...badStocks,
+  ...defaltStocks,
+  ...growthStocks,
+  ...blueChipStocks,
+  ...startupStocks,
+];
 
 // Map을 사용하여 중복 제거
 const stockMap = new Map<string, StockInfo>();
