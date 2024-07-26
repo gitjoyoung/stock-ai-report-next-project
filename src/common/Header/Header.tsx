1c40c357da61b2b8c9e10a66f6f1a6797c405eb0
Jo Young <githuborigin@gmail.com>
2024-07-26 11:48:23 +0900

    style : 헤더 폰트 색 변경

--- src/common/Header/Header.tsx
+++ src/common/Header/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header className="mb-3 px-5 flex justify-start items-center bg-grayscale-0 h-[64px]">
       <Link href="/">
-        <HeadingFont level="4" weight="bold">
+        <HeadingFont level="4" weight="bold" className="text-primary-800">
           해투비
         </HeadingFont>
       </Link>
