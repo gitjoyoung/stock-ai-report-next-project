3fa81845eccc2edb125bf4c47fb9355ee95353bc
Jo Young <githuborigin@gmail.com>
2024-07-26 09:10:05 +0900

    fix : 파이어베이스 공개키 수정

--- src/app/report/[id]/page.tsx
+++ src/app/report/[id]/page.tsx
@@ -12,14 +12,14 @@ export default async function page({ params }: Props) {
   const { id } = params;
   const deCodeId = await decodeURIComponent(id);
 
-  await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/report/stockList`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(mockStockDataList),
-    cache: 'no-cache',
-  }); // 검색 리스트 db에 주식 리스트 추가
+  // await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/report/stockList`, {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify(mockStockDataList),
+  //   cache: 'no-cache',
+  // }); // 검색 리스트 db에 주식 리스트 추가
 
   const fetchStockList = await fetch(
     `${
